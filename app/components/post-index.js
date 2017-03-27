626d205450d4808a4fb51f54bab0cc9e8fc3458c
akgoode <akgoode@gmail.com>
2017-03-27 16:14:32 -0400

    saving changes

--- app/components/post-index.js
+++ app/components/post-index.js
@@ -4,6 +4,9 @@ export default Ember.Component.extend({
   actions: {
     deletePost () {
       this.sendAction('deletePost', this.get('post'));
+    },
+    editPost () {
+      this.sendAction('editPost', this.get('post'));
     }
   }
 });
