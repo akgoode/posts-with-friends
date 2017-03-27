5512f1f3d4dee37131c4f815ce016284f9f1f1d6
akgoode <akgoode@gmail.com>
2017-03-27 16:37:35 -0400

    Adds the editable property to post model
    This property allows the handlebars templates to check for the
    ability to edit the resource

--- app/models/post.js
+++ app/models/post.js
@@ -1,19 +1,8 @@
 import DS from 'ember-data';
-import Ember from 'ember';
 
 export default DS.Model.extend({
-  auth: Ember.inject.service(),
-  isAuthenticated: Ember.computed.alias('auth.isAuthenticated'),
-  userId: JSON.parse(window.localStorage.getItem('storage:auth')).id,
   title: DS.attr('string'),
   content: DS.attr('string'),
   user: DS.belongsTo('user'),
-  belongsToUser: Ember.computed('userId', 'user', function () {
-    if (this.get('userId') === this.get('user')) {
-      return true;
-    }
-    console.log(this.get('userId'));
-    console.log(this.get('user'));
-    return false;
-  })
+  editable: DS.attr('boolean')
 });
