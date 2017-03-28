0bcbe02580bb25186d4b3475c291847bc32fc9f8
akgoode <akgoode@gmail.com>
2017-03-28 09:21:14 -0400

    Start creating a table to display the index
    Also puts required in the input fields for creating

--- app/routes/posts.js
+++ app/routes/posts.js
@@ -3,11 +3,9 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   auth: Ember.inject.service(),
   isAuthenticated: Ember.computed.alias('auth.isAuthenticated'),
-  userId: JSON.parse(window.localStorage.getItem('storage:auth')).id,
 
   model () {
     if(this.get('isAuthenticated')) {
-      console.log(this.get('userId'));
       return this.get('store').findAll('post');
     } else {
       this.transitionTo('index');
