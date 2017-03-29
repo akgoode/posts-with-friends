6da7745a9cc138ce5bcb027f5402d245e12aab46
akgoode <akgoode@gmail.com>
2017-03-29 12:39:00 -0400

    Adds a wildcard route to handle random URLs

--- app/router.js
+++ app/router.js
@@ -16,6 +16,7 @@ Router.map(function () {
   this.route('edit', { path: '/posts/:post_id/edit'});
   this.route('unauthorized');
   this.route('myposts');
+  this.route('page-not-found', { path: '/*wildcard' });
 });
 
 export default Router;
